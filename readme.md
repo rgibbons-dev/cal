854afa8f69f04326235d4000f014e92d4f8b228a
ryan gibbons <amalgam.drawers.0k@icloud.com>
2023-11-16 22:13:01 -0500

    added option to select days

---
+++ readme.md
@@ -0,0 +1,7 @@
+# cal
+
+cal is a 30-day calendar.
+
+it's quite simple.
+it just displays the next 30 days, today inclusive.
+you can click on each day, and it'll tell you what days you've clicked on
--- script.js
+++ script.js
@@ -1,35 +1,94 @@
+const ticketOptions = [
+  { type: 'Round Trip', price: 10, period: 1 },
+  { type: 'Weekly Pass', price: 43.50, period: 7 },
+  { type: 'Flex Pass', price: 80, period: 30 },
+];
+
+function calculateOptimalTicket(selectedDays) {
+  let minCost = Infinity;
+  let optimalTicket = null;
+
+  for (let ticket of ticketOptions) {
+      let requiredTickets = Math.ceil(selectedDays.length / ticket.period);
+      let totalCost = requiredTickets * ticket.price;
+
+      if (totalCost < minCost) {
+          minCost = totalCost;
+          optimalTicket = ticket.type;
+      }
+  }
+
+  return optimalTicket;
+}
+
+function displayOptimalTicket(ticketType) {
+  const recommendationElement = document.getElementById('ticket-recommendation');
+  recommendationElement.textContent = 'Recommended Ticket: ' + ticketType;
+}
+
+const selectedDays = [];
+
+function updateSelectedDaysDisplay() {
+  const displayElement = document.getElementById('selected-days');
+  if (selectedDays.length > 0) {
+    displayElement.textContent = 'Selected Days: ' + selectedDays.join(', ');
+  } else {
+    displayElement.textContent = 'Selected Days: None';
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    const daysContainer = document.querySelector('.days');
-    let currentDate = new Date();
-    const endDay = new Date(currentDate);
-    endDay.setDate(endDay.getDate() + 30);
+  const daysContainer = document.querySelector('.days');
+  let currentDate = new Date();
+  const endDay = new Date(currentDate);
+  endDay.setDate(endDay.getDate() + 30);
 
-    init = true;
+  init = true;
 
-    while(currentDate < endDay) {
-        if(init) {
-          const startDay = currentDate.getDay();
-          for(let i = 0; i < startDay; i++) {
-            const span = document.createElement('span');
-            daysContainer.appendChild(span);
-          }
-          init = false;
-        }
-
+  while(currentDate < endDay) {
+    if(init) {
+      const startDay = currentDate.getDay();
+      for(let i = 0; i < startDay; i++) {
         const span = document.createElement('span');
-        span.textContent = currentDate.getDate();
-
-        if (currentDate.toDateString() === new Date().toDateString()) {
-            span.classList.add('today');
-        }
-
         daysContainer.appendChild(span);
-
-        const br = document.createElement('br');
-        if(currentDate.getDay() === 6) {
-          daysContainer.appendChild(br);
-        }
-        currentDate.setDate(currentDate.getDate() + 1);
+      }
+      init = false;
     }
+
+    const span = document.createElement('span');
+    span.textContent = currentDate.getDate();
+
+    span.addEventListener('click', function() {
+      this.classList.toggle('selected');
+
+      const dayNumber = parseInt(this.textContent, 10);
+
+      if (this.classList.contains('selected')) {
+        selectedDays.push(dayNumber);
+      } else {
+        selectedDays = selectedDays.filter(day => day !== dayNumber);
+      }
+      
+      updateSelectedDaysDisplay();
+      let optimalTicket = calculateOptimalTicket(selectedDays);
+      displayOptimalTicket(optimalTicket);
+    });
+
+    if (currentDate.toDateString() === new Date().toDateString()) {
+      span.classList.add('today');
+    }
+
+    if(currentDate.getDay() === 6 || currentDate.getDay() === 0) {
+      span.classList.add('weekend');
+    }
+
+    daysContainer.appendChild(span);
+
+    const br = document.createElement('br');
+    if(currentDate.getDay() === 6) {
+      daysContainer.appendChild(br);
+    }
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
 });
 
